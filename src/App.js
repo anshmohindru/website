0b99ba12b5be9118664888fa574e0bd27294493b
anshmohindru <anshmohindru@gmail.com>
2025-04-14 15:57:00 -0700

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello, I’m Ansh 👋</h1>
       <p>Welcome to my personal website built with React.</p>
     </div>
